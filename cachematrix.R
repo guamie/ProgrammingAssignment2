b7f589914eba68d5a01908bb7141da42d327ab9b
Guam Lelananthachat <guamie@gmail.com>
2016-03-20 13:44:49 +0000

    New cachematrix 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,24 @@
 ## Coursera : Data Science : R-Programming 
 ## Week 3 Assignment : Caching the Inverse of a Matrix
-## Github : github.com/guamie
+## Github : https://github.com/guamie/ProgrammingAssignment2
+## Date: 20th Mar 2016
 
 ## Function : makeCacheMatrix
-## The function creates a matrix object to cache the inverse
+## The function creates a special matrix object to cache the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 	
-	cInverse <- NULL	## Initialize cInverse to store inverse matrix 
+	cInverse <- NULL								## Initialize cInverse to store inverse matrix 
 
-	set <— function(y) {	## Define function to assign new matrix value in parent environment
+	set <— function(y) {								## Define function to assign new matrix value in parent environment
 		x <<- y
-		cInverse <— NULL
+		cInverse <<- NULL
 	}
 
 	get <- function() x    ## Define Function to return matrix value
 
-	setInverse <- function(inverseValue) cInverse <<- inverseValue		## Set inverse value in parent environment
-	getInverse <- function cInverse               				## Get inverse value in parent environment
+	setInverse <- function(inverseValue) cInverse <<- inverseValue			## Set inverse value in parent environment
+	getInverse <- function cInverse               					## Get inverse value in parent environment
 	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
@@ -29,7 +30,8 @@ cacheSolve <- function(x, ...) {
 	
 	cInverse <- x$getinverse()
 
-	if(!is.null(cInverse)) {			## Checks to see if cInverse is not NULL			
+	if(!is.null(cInverse)) {							## Checks to see if cInverse is not NULL			
+		message("getting cached data")
 		return(cInverse)
 	}
 
